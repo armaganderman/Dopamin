0c21ae16230092358dbf393248b9f3cffb8bef1f
Armagan DERMAN <haderman@hotmail.com>
2022-11-08 08:50:58 +0300

    Created the project in .NET Framework 4.6.1
    This application meant to give an idea about some of the probability distributions and statistical formulas behind it. .NET 6 do not support chart control anymore. So instead .NET Framework 4.6.1 is used. You may create whatever UI you want.

--- Dopamin.Statistics/Dopamin.Statistics.WinformsUI/frmMain.Designer.cs
+++ Dopamin.Statistics/Dopamin.Statistics.WinformsUI/frmMain.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Dopamin.Statistics.Winforms
+﻿namespace Dopamin.Statistics.WinformsUI
 {
     partial class frmMain
     {
@@ -28,64 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSkewed = new System.Windows.Forms.Button();
+            this.btnNormal = new System.Windows.Forms.Button();
             this.btnUniform = new System.Windows.Forms.Button();
             this.btnTriangular = new System.Windows.Forms.Button();
-            this.btnNormal = new System.Windows.Forms.Button();
-            this.btnLognormal = new System.Windows.Forms.Button();
-            this.btnSkewed = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnUniform
+            // btnSkewed
             // 
-            this.btnUniform.Location = new System.Drawing.Point(12, 12);
-            this.btnUniform.Name = "btnUniform";
-            this.btnUniform.Size = new System.Drawing.Size(255, 23);
-            this.btnUniform.TabIndex = 0;
-            this.btnUniform.Text = "Uniform Distribution";
-            this.btnUniform.UseVisualStyleBackColor = true;
-            // 
-            // btnTriangular
-            // 
-            this.btnTriangular.Location = new System.Drawing.Point(12, 41);
-            this.btnTriangular.Name = "btnTriangular";
-            this.btnTriangular.Size = new System.Drawing.Size(255, 23);
-            this.btnTriangular.TabIndex = 1;
-            this.btnTriangular.Text = "Triangular Distribution";
-            this.btnTriangular.UseVisualStyleBackColor = true;
+            this.btnSkewed.Location = new System.Drawing.Point(12, 99);
+            this.btnSkewed.Name = "btnSkewed";
+            this.btnSkewed.Size = new System.Drawing.Size(220, 23);
+            this.btnSkewed.TabIndex = 9;
+            this.btnSkewed.Text = "Skewed Normal Distribution";
+            this.btnSkewed.UseVisualStyleBackColor = true;
+            this.btnSkewed.Click += new System.EventHandler(this.btnSkewed_Click);
             // 
             // btnNormal
             // 
             this.btnNormal.Location = new System.Drawing.Point(12, 70);
             this.btnNormal.Name = "btnNormal";
-            this.btnNormal.Size = new System.Drawing.Size(255, 23);
-            this.btnNormal.TabIndex = 2;
+            this.btnNormal.Size = new System.Drawing.Size(220, 23);
+            this.btnNormal.TabIndex = 8;
             this.btnNormal.Text = "Normal Distribution";
             this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
             // 
-            // btnLognormal
+            // btnUniform
             // 
-            this.btnLognormal.Location = new System.Drawing.Point(12, 99);
-            this.btnLognormal.Name = "btnLognormal";
-            this.btnLognormal.Size = new System.Drawing.Size(255, 23);
-            this.btnLognormal.TabIndex = 3;
-            this.btnLognormal.Text = "Lognormal Distribution";
-            this.btnLognormal.UseVisualStyleBackColor = true;
+            this.btnUniform.Location = new System.Drawing.Point(12, 12);
+            this.btnUniform.Name = "btnUniform";
+            this.btnUniform.Size = new System.Drawing.Size(220, 23);
+            this.btnUniform.TabIndex = 7;
+            this.btnUniform.Text = "Uniform Distribution";
+            this.btnUniform.UseVisualStyleBackColor = true;
+            this.btnUniform.Click += new System.EventHandler(this.btnUniform_Click);
             // 
-            // btnSkewed
+            // btnTriangular
             // 
-            this.btnSkewed.Location = new System.Drawing.Point(12, 128);
-            this.btnSkewed.Name = "btnSkewed";
-            this.btnSkewed.Size = new System.Drawing.Size(255, 23);
-            this.btnSkewed.TabIndex = 4;
-            this.btnSkewed.Text = "Skewed Normal Distribution";
-            this.btnSkewed.UseVisualStyleBackColor = true;
+            this.btnTriangular.Location = new System.Drawing.Point(12, 41);
+            this.btnTriangular.Name = "btnTriangular";
+            this.btnTriangular.Size = new System.Drawing.Size(220, 23);
+            this.btnTriangular.TabIndex = 6;
+            this.btnTriangular.Text = "Triangular Distribution";
+            this.btnTriangular.UseVisualStyleBackColor = true;
+            this.btnTriangular.Click += new System.EventHandler(this.btnTriangular_Click);
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(196, 172);
+            this.btnClose.Location = new System.Drawing.Point(157, 187);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(71, 23);
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 5;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -93,17 +87,16 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(279, 206);
-            this.Controls.Add(this.btnClose);
+            this.ClientSize = new System.Drawing.Size(244, 222);
             this.Controls.Add(this.btnSkewed);
-            this.Controls.Add(this.btnLognormal);
             this.Controls.Add(this.btnNormal);
-            this.Controls.Add(this.btnTriangular);
             this.Controls.Add(this.btnUniform);
+            this.Controls.Add(this.btnTriangular);
+            this.Controls.Add(this.btnClose);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
             this.Name = "frmMain";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -114,11 +107,10 @@
 
         #endregion
 
-        private Button btnUniform;
-        private Button btnTriangular;
-        private Button btnNormal;
-        private Button btnLognormal;
-        private Button btnSkewed;
-        private Button btnClose;
+        private System.Windows.Forms.Button btnSkewed;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnUniform;
+        private System.Windows.Forms.Button btnTriangular;
+        private System.Windows.Forms.Button btnClose;
     }
 }
---
+++ Dopamin.Statistics/Dopamin.Statistics.WinformsUI/frmMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dopamin.Statistics.WinformsUI
+{
+    public partial class frmMain : Form
+    {
+        #region Constructor
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Buttons
+
+        private void btnUniform_Click(object sender, System.EventArgs e)
+        {
+            var fUniformDistribution = new frmUniformDistribution();
+            fUniformDistribution.Show();
+        }
+
+        private void btnTriangular_Click(object sender, System.EventArgs e)
+        {
+            var fTriangularDistribution = new frmTriangularDistribution();
+            fTriangularDistribution.Show();
+        }
+
+        private void btnNormal_Click(object sender, System.EventArgs e)
+        {
+            var fNormalDistribution = new frmNormalDistribution();
+            fNormalDistribution.Show();
+        }
+
+        private void btnSkewed_Click(object sender, System.EventArgs e)
+        {
+            var fSkewedNormalDistribution = new frmSkewedNormalDistribution();
+            fSkewedNormalDistribution.Show();
+        }
+
+        private void btnClose_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+    }
+}
